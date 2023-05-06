311064d2590e248a3ff02acbb2bc862514b18d93
ikaera <ikaera@gmail.com>
2023-05-06 14:47:17 -0400

    add.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -46,3 +46,15 @@ export const REMOVE_BOOK = gql`
     }
   }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
